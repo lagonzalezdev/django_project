76aa8324d8b0592edf29fa2feb6b4c3e3e35b747
#lagonzalezdev <gonzalezortizlau@gmail.com>
2023-02-14 15:19:46 -0500

    migration and url

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'PortfolioLagonzalezConfig'
+    'portfolio_lagonzalez'
 ]
 
 MIDDLEWARE = [
--- portfolio_lagonzalez/models.py
+++ portfolio_lagonzalez/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.db.models.fields import CharField
-from django.db.models.fields.files import URLField
+from django.forms import URLField
+
+
 # Create your models here.
 class Project(models.Model):
     title = CharField(max_length=100)
     description = CharField(max_length=250)
-    lenguage = CharField(max_length= 100)
-    url_github = URLField(blank=True)
-    url_deploy = URLField(blank=True)
+    lenguage = CharField(max_length=100)
+    url_github = URLField()
+    url_deploy = URLField()
