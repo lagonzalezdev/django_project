d06dfb147fedb3b402b361b2c1c38fede5f837ec
#lagonzalezdev <gonzalezortizlau@gmail.com>
2023-02-15 16:47:17 -0500

    add images-style sheet

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from portfolio_lagonzalez import views
 
@@ -22,3 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name='index'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
