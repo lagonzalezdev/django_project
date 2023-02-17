e534e516677894663bfbe1cbb0ae86f7738ec114
#lagonzalezdev <gonzalezortizlau@gmail.com>
2023-02-17 17:36:03 -0500

    update test.py

--- portfolio_lagonzalez/tests.py
+++ portfolio_lagonzalez/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Project
 
-# Create your tests here.
+class ProjectTest(TestCase):
+    def setUp(self):
+        Project.objects.create(title="CharField", description="CharField")
+
+    def test_project(self):
+        self.assertEqual(Project.objects.count(), 1)
+        obj = Project.objects.get(title="CharField")
+        self.assertEqual(obj.title, "CharField")
